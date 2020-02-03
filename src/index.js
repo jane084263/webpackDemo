6c0418fe2aba15faf621a2d1f7ad3b4bb7c84be1
孙霖霖 <sunlinlin084263@163.com>
2020-02-03 14:51:53 +0800

    图片处理fileLoader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from "lodash";
 import "./style.css";
-
+import Cute from "./cute.jpg";
 function component() {
   var element = document.createElement("div");
   element.innerHTML = _.join(["hello", "webpack"], " ");
   element.classList.add("hello");
+  var myIcon = new Image();
+  myIcon.src = Cute;
+  element.appendChild(myIcon);
   return element;
 }
 
