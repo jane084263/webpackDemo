daa60312569a57e04a822490791f966a7fcc6a34
孙霖霖 <sunlinlin084263@163.com>
2020-02-09 16:46:56 +0800

    清理dist文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -13,6 +14,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "outPutManagement"
-    })
+    }),
+    new CleanWebpackPlugin()
   ]
 };
