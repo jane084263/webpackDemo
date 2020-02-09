df596e9fbe7d81e65c36fc66a9f06ae007a27a12
孙霖霖 <sunlinlin084263@163.com>
2020-02-09 13:04:45 +0800

    回退处理-管理输出

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from "lodash";
-import "./style.css";
-import Cute from "./cute.jpg";
-import Data from "./data.xml";
 
 function component() {
   var element = document.createElement("div");
   element.innerHTML = _.join(["hello", "webpack"], " ");
   element.classList.add("hello");
-  var myIcon = new Image();
-  myIcon.src = Cute;
-
-  var spanEL = document.createElement("span");
-  spanEL.innerHTML = "&#xe720;";
-  spanEL.classList.add("m_buy");
-
-  element.appendChild(myIcon);
-  element.appendChild(spanEL);
-  console.log(Data);
   return element;
 }
 
