8eca3be096233df12ee47e8ca31fa5c76509eba9
孙霖霖 <sunlinlin084263@163.com>
2020-02-18 15:04:42 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
-    print: "./src/print.js"
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    app: "./src/index.js"
   },
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+    // hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "outPutManagement"
     }),
-    new CleanWebpackPlugin()
-  ]
+    new CleanWebpackPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
