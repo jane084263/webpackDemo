b3ffe941efcfd094780c27f8de2b60930d67d3dc
sunlinlin <sunlinlin635@pingan.com.cn>
2020-02-20 18:24:37 +0800

    catching -- extracting boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,8 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/"
+  },
+  optimization: {
+    runtimeChunk: 'single',
   }
 };
